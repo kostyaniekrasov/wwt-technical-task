daf6f9299f97007fac9a81afc9119dc659b60f99
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2025-04-02 15:32:17 +0300

    feat(i18n): add confirm modal translations in confirm.json

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -1,7 +1,9 @@
+import confirm from './confirm.json'
 import filter from './filter.json'
 import notFound from './not-found.json'
 
 export const en = {
 	filter: filter,
+	confirm: confirm,
 	'not-found': notFound
 } as const
